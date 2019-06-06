df318c2a796dfdd21a38c00036a6e9d832c940db
Tim B <tbatty7@gmail.com>
2019-06-06 14:19:17 -0400

    added serializer and test

--- src/main/java/com/battybuilds/kafkastreams/avro/model/AvroSerDes.java
+++ src/main/java/com/battybuilds/kafkastreams/avro/model/AvroSerDes.java
@@ -1,4 +1,28 @@
 package com.battybuilds.kafkastreams.avro.model;
 
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.ByteArrayOutputStream;
+
 public class AvroSerDes {
+
+    public byte[] serealizeAvroHttpRequestJSON(AvroHttpRequest request) {
+        DatumWriter<AvroHttpRequest> writer = new SpecificDatumWriter<>(AvroHttpRequest.class);
+        byte[] data = new byte[0];
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        Encoder jsonEncoder = null;
+        try {
+            jsonEncoder = EncoderFactory.get().jsonEncoder(AvroHttpRequest.getClassSchema(), stream);
+            writer.write(request, jsonEncoder);
+            jsonEncoder.flush();
+            data = stream.toByteArray();
+        } catch (Exception e) {
+            System.out.println("Serialization Error: " + e.getMessage());
+        }
+
+        return data;
+    }
 }
--- src/test/java/com/battybuilds/kafkastreams/avro/model/AvroSerDesTest.java
+++ src/test/java/com/battybuilds/kafkastreams/avro/model/AvroSerDesTest.java
@@ -1,5 +1,35 @@
+package com.battybuilds.kafkastreams.avro.model;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class AvroSerDesTest {
+    @Test
+    public void canSerializeRequestToByteArray() {
+        AvroHttpRequest request = createAvroHttpRequest();
+        byte[] bytes = serialize(request);
+        assertEquals(141, bytes.length);
+    }
 
+    private byte[] serialize(AvroHttpRequest request) {
+        AvroSerDes avroSerDes = new AvroSerDes();
+        return avroSerDes.serealizeAvroHttpRequestJSON(request);
+    }
+
+    private AvroHttpRequest createAvroHttpRequest() {
+        ClientIdentifier clientIdentifier = ClientIdentifier.newBuilder()
+                .setHostName("hostName")
+                .setIpAddress("127.0.0.1")
+                .build();
+
+        return AvroHttpRequest.newBuilder()
+                .setClientIdentifier(clientIdentifier)
+                .setEmployeeNames(Arrays.asList("Tim", "Jessica", "Mike"))
+                .setRequestTime(10L)
+                .setActive(Active.YES)
+                .build();
+    }
 }
