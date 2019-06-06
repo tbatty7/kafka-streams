d623b351556e1fb6dd2e6c6e7751acea21843f24
Tim B <tbatty7@gmail.com>
2019-06-06 11:02:37 -0400

    added util class to generate schema file in json format with test

--- src/main/java/com/battybuilds/kafkastreams/AvroSchemaBuilder.java
+++ src/main/java/com/battybuilds/kafkastreams/AvroSchemaBuilder.java
@@ -1,4 +1,21 @@
 package com.battybuilds.kafkastreams;
 
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+
 public class AvroSchemaBuilder {
+
+    public Schema createAvroHttpRequestSchema(){
+
+        Schema clientIdentifier = SchemaBuilder.record("ClientIdentifier").namespace("com.battybuilds.kafkastreams.avro.model")
+                .fields().requiredString("hostName").requiredString("ipAddress").endRecord();
+
+        Schema avroHttpRequest = SchemaBuilder.record("AvroHttpRequest").namespace("com.battybuilds.kafkastreams.avro.model").fields()
+                .requiredLong("requestTime")
+                .name("clientIdentifier").type(clientIdentifier).noDefault()
+                .name("employeeNames").type().array().items().stringType().arrayDefault(null)
+                .name("active").type().enumeration("Active").symbols("YES", "NO").noDefault()
+                .endRecord();
+        return avroHttpRequest;
+    }
 }
--- src/test/java/com/battybuilds/kafkastreams/AvroSchemaBuilderTest.java
+++ src/test/java/com/battybuilds/kafkastreams/AvroSchemaBuilderTest.java
@@ -1,5 +1,15 @@
+package com.battybuilds.kafkastreams;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class AvroSchemaBuilderTest {
 
+    @Test
+    public void createAvroHttpRequestSchema() {
+        AvroSchemaBuilder avroSchemaBuilder = new AvroSchemaBuilder();
+        String schema = avroSchemaBuilder.createAvroHttpRequestSchema().toString();
+        System.out.println(schema);
+    }
 }
