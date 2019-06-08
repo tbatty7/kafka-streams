d354f7fcb09dae89a5be51af79ee36b18d7ed37d
Tim B <tbatty7@gmail.com>
2019-06-07 23:21:30 -0400

    autowired deserializer into constructor

--- src/main/java/com/battybuilds/kafkastreams/Consumer.java
+++ src/main/java/com/battybuilds/kafkastreams/Consumer.java
@@ -1,21 +1,26 @@
 package com.battybuilds.kafkastreams;
 
+import com.battybuilds.kafkastreams.avro.model.AvroHttpRequest;
 import com.battybuilds.kafkastreams.utils.AvroSerDes;
 import com.battybuilds.kafkastreams.utils.MessageStreams;
-import com.battybuilds.kafkastreams.avro.model.AvroHttpRequest;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 
 @EnableBinding(MessageStreams.class)
 public class Consumer {
 
-    AvroSerDes serDes = new AvroSerDes();
+    private final AvroSerDes serDes;
+
+    public Consumer(AvroSerDes serDes) {
+        this.serDes = serDes;
+    }
 
     @StreamListener(MessageStreams.STREAM_ONE)
     public void streamHandler(byte[] messageRequest) {
         AvroHttpRequest request = serDes.deserializeBinary(messageRequest);
         System.out.println("****************MESSAGE RECEIVED BELOW***************************");
         System.out.println(new String(messageRequest));
+
         System.out.println(request.toString());
     }
 }
