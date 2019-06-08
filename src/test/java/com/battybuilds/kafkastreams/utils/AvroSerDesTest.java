d3565626ef5a296783a4dc4378ed6d9e072f5a45
Tim B <tbatty7@gmail.com>
2019-06-07 22:58:08 -0400

    refactored tests

--- src/test/java/com/battybuilds/kafkastreams/utils/AvroSerDesTest.java
+++ src/test/java/com/battybuilds/kafkastreams/utils/AvroSerDesTest.java
@@ -1,11 +1,9 @@
 package com.battybuilds.kafkastreams.utils;
 
 import com.battybuilds.kafkastreams.avro.model.AvroHttpRequest;
-import com.battybuilds.kafkastreams.avro.model.ClientIdentifier;
 import org.junit.Test;
 
-import java.util.Arrays;
-
+import static com.battybuilds.kafkastreams.utils.RequestUtils.createAvroHttpRequest;
 import static org.junit.Assert.assertEquals;
 
 public class AvroSerDesTest {
@@ -43,16 +41,5 @@ public class AvroSerDesTest {
         return request.getClientIdentifier().getHostName();
     }
 
-    private AvroHttpRequest createAvroHttpRequest() {
-        ClientIdentifier clientIdentifier = ClientIdentifier.newBuilder()
-                .setHostName("hostName")
-                .setIpAddress("127.0.0.1")
-                .build();
 
-        return AvroHttpRequest.newBuilder()
-                .setClientIdentifier(clientIdentifier)
-                .setEmployeeNames(Arrays.asList("Tim", "Jessica", "Mike"))
-                .setRequestTime(10L)
-                .build();
-    }
 }
--- src/test/java/com/battybuilds/kafkastreams/utils/RequestUtils.java
+++ src/test/java/com/battybuilds/kafkastreams/utils/RequestUtils.java
@@ -1,4 +1,22 @@
 package com.battybuilds.kafkastreams.utils;
 
+import com.battybuilds.kafkastreams.avro.model.AvroHttpRequest;
+import com.battybuilds.kafkastreams.avro.model.ClientIdentifier;
+
+import java.util.Arrays;
+
 public class RequestUtils {
+
+    public static AvroHttpRequest createAvroHttpRequest() {
+        ClientIdentifier clientIdentifier = ClientIdentifier.newBuilder()
+                .setHostName("hostName")
+                .setIpAddress("127.0.0.1")
+                .build();
+
+        return AvroHttpRequest.newBuilder()
+                .setClientIdentifier(clientIdentifier)
+                .setEmployeeNames(Arrays.asList("Tim", "Jessica", "Mike"))
+                .setRequestTime(10L)
+                .build();
+    }
 }
