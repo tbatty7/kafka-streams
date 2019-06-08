7ebfa0ca2a4a6860a0f77d87789605f4f10a7aef
Tim B <tbatty7@gmail.com>
2019-06-07 21:03:45 -0400

    wired up serializer with request correctly but with null request object

--- src/main/java/com/battybuilds/kafkastreams/ProducerController.java
+++ src/main/java/com/battybuilds/kafkastreams/ProducerController.java
@@ -1,5 +1,6 @@
 package com.battybuilds.kafkastreams;
 
+import com.battybuilds.kafkastreams.avro.model.AvroHttpRequest;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,9 @@ public class ProducerController {
     @GetMapping("/send")
     public String sendMessage() {
 
-        String messageString = "hello";
-        serDes.serealizeAvroHttpRequestBinary(messageString);
-        messageChannels.outputStream().send(MessageBuilder.withPayload(messageString).build());
+        AvroHttpRequest request = null;
+        byte[] serializedRequest = serDes.serealizeAvroHttpRequestBinary(request);
+        messageChannels.outputStream().send(MessageBuilder.withPayload(serializedRequest).build());
         System.out.println("Message Sent!");
         return "order_published";
     }
