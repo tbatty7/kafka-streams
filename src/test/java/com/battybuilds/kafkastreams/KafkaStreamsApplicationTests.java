ee652a43912f5ff7b34d0654a9db49059823c1c7
Tim B <tbatty7@gmail.com>
2019-06-18 11:18:03 -0400

    refactored integration test to ready it for possible integration tests to detect topics wired up correctly

--- src/test/java/com/battybuilds/kafkastreams/KafkaStreamsApplicationTests.java
+++ src/test/java/com/battybuilds/kafkastreams/KafkaStreamsApplicationTests.java
@@ -1,17 +1,31 @@
 package com.battybuilds.kafkastreams;
 
+import com.battybuilds.kafkastreams.avro.model.AvroHttpRequest;
+import com.battybuilds.kafkastreams.utils.AvroSerDes;
+import com.battybuilds.kafkastreams.utils.MessageStreams;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.battybuilds.kafkastreams.utils.RequestUtils.createAvroHttpRequest;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KafkaStreamsApplicationTests {
 
+	@Autowired
+	ProducerController producerController;
+
 	@Test
-	public void contextLoads() {
+	public void receivesMsgForAwesomeTopicFromKafka() {
+		producerController.sendMessage();
+
 	}
 
 }
