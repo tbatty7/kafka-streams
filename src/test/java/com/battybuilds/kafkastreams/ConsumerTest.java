7c11c676217dc8975d371db06f44aa565eecfa1e
Tim B <tbatty7@gmail.com>
2019-06-18 11:18:27 -0400

    created file for new tests for Consumer

--- src/test/java/com/battybuilds/kafkastreams/ConsumerTest.java
+++ src/test/java/com/battybuilds/kafkastreams/ConsumerTest.java
@@ -1,5 +1,15 @@
+package com.battybuilds.kafkastreams;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class ConsumerTest {
+    @Test
+    public void testMePlease() {
 
+    }
 }
