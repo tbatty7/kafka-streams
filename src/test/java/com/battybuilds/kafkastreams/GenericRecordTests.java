d73664e70081dff7c75c059cc097b512694185aa
Tim B <tbatty7@gmail.com>
2019-06-24 10:11:29 -0400

    added tests showing how to create a generic vs a specific record

---
+++ src/test/java/com/battybuilds/kafkastreams/GenericRecordTests.java
@@ -0,0 +1,41 @@
+package com.battybuilds.kafkastreams;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class GenericRequestTests {
+
+    @Test
+    public void createGenericRecord() {
+        Schema.Parser parser = new Schema.Parser();
+        Schema schema = null;
+        try {
+            schema = parser.parse(new File("src/main/resources/avroHttpRequest-schema.avsc"));
+            System.out.println("Schema");
+            System.out.println(schema.toString());
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        GenericRecordBuilder builder = new GenericRecordBuilder(schema.getField("clientIdentifier").schema());
+        GenericData.Record clientIdentifier = builder.set("hostName", "hostname")
+                .set("ipAddress", "127.0.0.1")
+                .build();
+
+        GenericData.Record record = new GenericRecordBuilder(schema)
+                .set("clientIdentifier", clientIdentifier)
+                .set("employeeNames", Arrays.asList("Tim","Robert","Bill"))
+                .set("requestTime", 10L)
+                .build();
+        System.out.printf("Full record\n");
+        System.out.println(record.toString());
+    }
+
+
+}
